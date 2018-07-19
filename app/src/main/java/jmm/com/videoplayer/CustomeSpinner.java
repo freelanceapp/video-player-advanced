25e5ce6add4b9361b467d3933568d2ed63decfb5
PrernaYadav <prernasyadav027@gmail.com>
2018-07-19 15:42:08 +0530

    set video folder in spinner

---
+++ app/src/main/java/jmm/com/videoplayer/CustomeSpinner.java
@@ -0,0 +1,39 @@
+package jmm.com.videoplayer;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomeSpinner extends ArrayAdapter<String> {
+    private List<String> objects;
+
+    public CustomeSpinner(Context context, int textViewResourceId, List<String> objects) {
+        super(context, textViewResourceId, objects);
+        this.objects = objects;
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, @NonNull ViewGroup parent) {
+        return getCustomView(position, convertView, parent);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        return getCustomView(position, convertView, parent);
+    }
+
+    private View getCustomView(final int position, View convertView, ViewGroup parent) {
+        View row = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_spinner, parent, false);
+        final TextView label = (TextView) row.findViewById(R.id.tv_spinnervalue);
+        label.setText(objects.get(position));
+        return row;
+    }
+}
---
+++ app/src/main/java/jmm/com/videoplayer/HomeActivity.java
@@ -0,0 +1,213 @@
+package jmm.com.videoplayer;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class HomeActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private String[] category = {};
+    private static final int REQUEST_PERMISSIONS = 100;
+    List<String> videoFolderNamearray = new ArrayList<>();
+    ArrayList<String> videoPatharray = new ArrayList<>();
+    ArrayList<String> data = new ArrayList<>();
+    Spinner navigationSpinner;
+    int column_index_data, column_index_folder_name, column_id, thum;
+    Toolbar toolbar;
+    String a;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+     /*   FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+
+        if ((ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) && (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
+            if ((ActivityCompat.shouldShowRequestPermissionRationale(HomeActivity.this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) && (ActivityCompat.shouldShowRequestPermissionRationale(HomeActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE))) {
+
+            } else {
+                ActivityCompat.requestPermissions(HomeActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_PERMISSIONS);
+            }
+        } else {
+            getfolders();
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, videoFolderNamearray);
+        navigationSpinner = new Spinner(getSupportActionBar().getThemedContext());
+        navigationSpinner.setAdapter(new CustomeSpinner(this, R.layout.custom_spinner, videoFolderNamearray));
+        toolbar.addView(navigationSpinner, 0);
+
+
+        navigationSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // your code here
+                a = String.valueOf(navigationSpinner.getSelectedItem());
+                Toast.makeText(HomeActivity.this, a, Toast.LENGTH_SHORT).show();
+
+                for (int i = 0; i < videoPatharray.size(); i++) {
+                    if (videoPatharray.get(i).contains("Android")) {
+                        Toast.makeText(HomeActivity.this, "fdfdfdfdfdfdfdfdfd", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        navigationView.setItemIconTintList(null);
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+        } else if (id == R.id.nav_settings) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_aboutus) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void getfolders() {
+
+        String[] projection = {MediaStore.Video.Thumbnails.DATA, MediaStore.Video.Media._ID, MediaStore.Video.VideoColumns.DATA, MediaStore.Video.Media.DISPLAY_NAME, MediaStore.Video.Media.BUCKET_DISPLAY_NAME};
+        Cursor cursor = this.getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, projection, null, null, null);
+
+        while (cursor.moveToNext()) {
+
+            column_index_data = cursor.getColumnIndexOrThrow(MediaStore.Video.VideoColumns.DATA);
+            column_index_folder_name = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.BUCKET_DISPLAY_NAME);
+            column_id = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID);
+            thum = cursor.getColumnIndexOrThrow(MediaStore.Video.Thumbnails.DATA);
+
+            data.add("" + MediaStore.Video.Media.BUCKET_DISPLAY_NAME);
+            ArrayList<String> name = new ArrayList<>(column_index_folder_name);
+            ArrayList<String> id = new ArrayList<>(column_id);
+            ArrayList<String> thumb = new ArrayList<>(thum);
+
+
+        }
+
+
+        try {
+            cursor.moveToFirst();
+
+            do {
+                videoFolderNamearray.add((cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.BUCKET_DISPLAY_NAME))));
+                videoPatharray.add((cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Video.VideoColumns.DATA))));
+                Log.e("prerna", String.valueOf(videoFolderNamearray));
+                Log.e("prerna", String.valueOf(videoPatharray));
+
+
+            } while (cursor.moveToNext());
+
+            cursor.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        ArrayList<String> downloadedList = new ArrayList<>(videoItemHashSet);
+
+
+    }
+
+}
