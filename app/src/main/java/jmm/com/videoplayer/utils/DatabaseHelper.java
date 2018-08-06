5b1e4174528adc6a6626aacb47a4638407550533
PrernaYadav <prernasyadav027@gmail.com>
2018-08-06 18:02:40 +0530

    database made

---
+++ app/src/main/java/jmm/com/videoplayer/utils/DatabaseHelper.java
@@ -0,0 +1,79 @@
+package jmm.com.videoplayer.utils;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "Favourite.db";
+    public static final String TABLE_NAME = "favourite_table";
+    public static final String ID = "id";
+    public static final String NAME = "name";
+    public static final String THUMB = "thumb";
+    public static final String FOLDER = "folder";
+    public static final String TIME = "time";
+    public static final String RESOLUTION = "resolution";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        sqLiteDatabase.execSQL("create table " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,THUMB TEXT,FOLDER TEXT, TIME TEXT,RESOLUTION TEXT)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    public boolean insertdata(String name,String thumb,String folder,String time,String resolution) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(NAME, name);
+        contentValues.put(THUMB, thumb);
+        contentValues.put(FOLDER, folder);
+        contentValues.put(TIME, time);
+        contentValues.put(RESOLUTION, resolution);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+
+        }
+    }
+
+    public Cursor getalldata() {
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("select * from " + TABLE_NAME, null);
+        return cursor;
+    }
+
+    public boolean updatedata(String id,String name) {
+
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(NAME, name);
+        contentValues.put(ID, id);
+        database.update(TABLE_NAME, contentValues, "ID = ?", new String[] { name });
+
+        return true;
+    }
+
+    public Integer deletedata(String name){
+        SQLiteDatabase sqLiteDatabase=this.getReadableDatabase();
+        return sqLiteDatabase.delete(TABLE_NAME,"NAME = ? ",new String[] {name});
+
+    }
+
+}
